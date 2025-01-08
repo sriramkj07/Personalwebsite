10c487275f99b63a5f2b9e3102de1d4c561030c0
sriramkj07 <128556537+sriramkj07@users.noreply.github.com>
2025-01-07 22:56:37 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-  },
-  assetPrefix: '/'
+  }
 }
+
+module.exports = nextConfig
