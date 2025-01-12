182983b21a1c562375783e099593214306fcd5ca
sriramkj07 <128556537+sriramkj07@users.noreply.github.com>
2025-01-11 23:55:10 -0800

    Update page.tsx for essays

--- app/Essays/page.tsx
+++ app/Essays/page.tsx
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: 'Essays'
+}
 
+export default function Essays() {
+  return (
+    <div className="min-h-screen bg-white text-black p-8 mdip-16 lg:p-24 font-mono">
+      <h1 className="text-3xl font-bold mb-6">Essays</h1>
+      {/* Some Influential Essays I personally Believe Will Be Timeless */}
+    </div>
+  )
+}
