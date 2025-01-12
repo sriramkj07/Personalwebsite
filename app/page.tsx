92d5c35fbca3a896e158b3fe0197481bb46bdda6
sriramkj07 <128556537+sriramkj07@users.noreply.github.com>
2025-01-12 01:06:00 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -57,10 +57,12 @@ I aspire to build such large scale platforms for the future.
 
 I hope to write about some of these interests and showcase my builds.
 
-I'm currently based in San Francisco and previously spent time in Boston (where I went to Grad school) as well as in Chennai and Hyderabad
+I'm currently based in San Francisco and previously spent time in Boston (where I went to Grad school) as well as in Chennai and Hyderabad</p>
 
-<Link href="/essays" className="text-blue-600 hover:text-blue-800">Essays</Link>: I also collect some essays that I personally think are influential and will be timeless
-          </p>
+          <div>
+            <Link href="/essays" className="text-blue-600 hover:text-blue-800">Essays</Link>
+            : I also collect some essays that I personally think are influential and will be timeless
+          </div>
         </div>
       </div>
     </div>
