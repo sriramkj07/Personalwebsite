aa3a8585f2ba917914293ccad3cd543c83078b04
sriramkj07 <128556537+sriramkj07@users.noreply.github.com>
2025-01-12 00:30:59 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Github, Linkedin, Mail, Calendar } from 'lucide-react'
-
+import Link from 'next/link'
 export default function Home() {
   return (
     <div className="min-h-screen bg-white text-black p-8 md:p-16 lg:p-24 font-mono">
@@ -47,21 +47,20 @@ export default function Home() {
           
           <div className="space-y-4 text-gray-600">
             <p style={{ whiteSpace: 'pre-line' }}>
-              Hi there 👋
+            'Hi there 👋
              I'm Sriram (Shri - Rahhm), I build products, I cycle, I love reading books and I have a great fondness for stand-up com(ics)edy.
               I currently build products at ServiceNow. I'm fascinated by powerful network effects enabled by tech and how it disrupts traditional markets.
               I aspire to build such large scale platforms for the future.
               I hope to write about some of these interests and showcase my builds. 
               
-              I'm currently based in San Francisco and previously spent time in Boston (where I went to Grad school) as well as in Chennai and Hyderabad
+              I'm currently based in San Francisco and previously spent time in Boston (where I went to Grad school) as well as in Chennai and Hyderabad'
+ <Link href="/essays" className="text-blue-600 hover:text-blue-800 flex items-center gap-2">
+           Essays: A collection of Essays that I personally keep going back and think will be timeless
+         </Link>
             </p>
 
-         import Link from 'next/link'
-// ...
-<div className="mt-4">
-  <Link href="/essays" className="text-blue-600 hover:text-blue-800">Essays</Link>
-  : A collection of Essays that I personally keep going back and think will be timeless
-</div>
+              
+
           </div>
         </div>
       </div>
