e6416b74a04c4db131475de883465d0ce3689788
sriramkj07 <128556537+sriramkj07@users.noreply.github.com>
2025-01-18 23:29:21 -0800

    Update page.tsx

--- app/yeartracker/components/page.tsx
+++ app/yeartracker/components/page.tsx
@@ -1,6 +1,7 @@
-import YearProgress from './components/YearProgress'
+import YearProgress from '@/app/yeartracker/components/YearProgress'
 
 export default function Page() {
   return <YearProgress />
 }
+
 
