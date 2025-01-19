6eb015a7fd092ca7f99dc4940ca01289ef6cb529
sriramkj07 <128556537+sriramkj07@users.noreply.github.com>
2025-01-18 22:16:31 -0800

    Create layout.tsx

---
+++ app/yeartracker/components/layout.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'Year Tracker',
+  description: 'Track the progress of the current year',
+}
+
+export default function YearTrackerLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
+
