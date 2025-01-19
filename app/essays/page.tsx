709a96c3c79c0a7318f63d120c4efd75294c939f
sriramkj07 <128556537+sriramkj07@users.noreply.github.com>
2025-01-18 21:17:31 -0800

    Update page.tsx

--- app/essays/page.tsx
+++ app/essays/page.tsx
@@ -16,7 +16,8 @@ export default function Essays() {
           These essays have a high re-reability rate. I personally have went back to look, read, get inspired by them several times.
         </p>
         <p className="text-gray-700 mb-4 leading-relaxed">
-          I also wanted a personal repository that I could just access immediately, and if it helps others get inspired even better! 
+          I also wanted a personal repository that I could just access immediately, and if it helps others get inspired, even better! </p>
+<p className="text-gray-700 mb-4 leading-relaxed"> 
 If you know of an interesting essay you've read that can breakdown some complex ideas into simple truths, drop me a note at <a href="mailto:kothandaraman.s@northeastern.edu" className="text-blue-600 underline">kothandaraman.s@northeastern.edu</a>.
         </p>
         <h2 className="text-2xl font-bold mb-4">Business</h2>
