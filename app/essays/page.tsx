b928f59cf54001f9ca59a9688ed72bb5790bd4e3
sriramkj07 <128556537+sriramkj07@users.noreply.github.com>
2025-01-18 21:12:47 -0800

    Update page.tsx

--- app/essays/page.tsx
+++ app/essays/page.tsx
@@ -10,13 +10,14 @@ export default function Essays() {
       <div className="max-w-3xl">
         <h1 className="text-3xl font-bold mb-6 text-center">Essays</h1>
         <p className="text-gray-700 mb-4 leading-relaxed">
-          I'm fascinated by interesting essays written by extremely smart people, which also immediately made sense and kept me hooked to my chair. 
+          I'm fascinated by interesting essays written by extremely smart people, which made sense and kept me hooked. 
         </p>
         <p className="text-gray-700 mb-4 leading-relaxed">
-          They also have a high re-reability rate, and I personally have went back to look, read, get inspired by them several times.
+          These essays have a high re-reability rate. I personally have went back to look, read, get inspired by them several times.
         </p>
         <p className="text-gray-700 mb-4 leading-relaxed">
-          I also wanted a personal repository that I could just access immediately, and if it helps others get inspired even better! If you know of an interesting essay you've read, drop me a note at <a href="mailto:kothandaraman.s@northeastern.edu" className="text-blue-600 underline">kothandaraman.s@northeastern.edu</a>.
+          I also wanted a personal repository that I could just access immediately, and if it helps others get inspired even better! 
+If you know of an interesting essay you've read that can breakdown some complex ideas into simple truths, drop me a note at <a href="mailto:kothandaraman.s@northeastern.edu" className="text-blue-600 underline">kothandaraman.s@northeastern.edu</a>.
         </p>
         <h2 className="text-2xl font-bold mb-4">Business</h2>
         <ul className="list-disc pl-8 text-gray-700 space-y-4">
